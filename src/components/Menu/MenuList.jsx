5e7f07badf88aaec7827e56ff1b820c28c79bc35
ArtemKotlar <artemkotlar93@gmail.com>
2023-01-25 03:47:52 +0200

    Menu

---
+++ src/components/Menu/MenuList.jsx
@@ -0,0 +1,16 @@
+import MenuItem from "./MenuItem"
+import { menuConfig } from "constants/menu"
+
+const MenuList = () => {
+    console.log(menuConfig)
+    return(<nav>
+        <ul>
+            {menuConfig.map(({ name, link, img }) =>
+                <MenuItem key={name} name={name} link={link} img={img}/>  
+            )}
+                
+            </ul>
+        </nav>)
+}
+
+export default MenuList
